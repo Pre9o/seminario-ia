d2193f26918eed2c8577d935d0440efb5582e4d4
pre9o <rcpregardier@inf.ufsm.br>
2026-02-04 15:14:06 -0300

    refactor: refactoring EDA to fill teh dataset using random forest

--- EDA/fill_missing_values.py
+++ EDA/fill_missing_values.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.impute import SimpleImputer
 
 def fill_missing_values(dataset_path, target_column):
@@ -25,40 +25,50 @@ def fill_missing_values(dataset_path, target_column):
     #     dataset = dataset.dropna(subset=cols_less_2pct_missing)
 
     # 5. processed_dataset recebe o dataset atualizado
-    processed_dataset = dataset
-    missing_values_after = {col: int(count) for col, count in processed_dataset.isnull().sum().items() if count > 0}
-    print("Missing values per column after processing:", missing_values_after)
+    continuous_columns = []
+    categorical_columns = []
 
-    for column in missing_values_after.keys():
-        flag_column = f'{column}_missing_flag'
-        processed_dataset[flag_column] = processed_dataset[column].isnull().astype(int)
+    for column in missing_values_dict.keys():
+        if dataset[column].nunique() < 15:
+            categorical_columns.append(column)
+        else:
+            continuous_columns.append(column)
 
-    for column in missing_values_after.keys():
-        df_missing = processed_dataset[processed_dataset[column].isnull()]
-        df_not_missing = processed_dataset[processed_dataset[column].notnull()]
+    print("Continuous columns with missing values:", continuous_columns)
+    print("Categorical columns with missing values:", categorical_columns)
+            
+    for column in continuous_columns:
+        model = RandomForestRegressor(n_estimators=100, random_state=42)
+        train_data = dataset[dataset[column].notnull()]
+        test_data = dataset[dataset[column].isnull()]
 
-        if df_missing.empty:
-            continue
+        X_train = train_data.drop(columns=[column, target_column])
+        y_train = train_data[column]
 
-        X_train = df_not_missing.drop(columns=[column, target_column])
-        y_train = df_not_missing[column]
-        X_pred = df_missing.drop(columns=[column, target_column])
+        X_test = test_data.drop(columns=[column, target_column])
 
-        cols_with_missing_in_X_train = X_train.columns[X_train.isnull().any()].tolist()
-        cols_with_missing_in_X_pred = X_pred.columns[X_pred.isnull().any()].tolist()
-        cols_to_drop = list(set(cols_with_missing_in_X_train) | set(cols_with_missing_in_X_pred))
-        X_train = X_train.drop(columns=cols_to_drop)
-        X_pred = X_pred.drop(columns=cols_to_drop)
+        model.fit(X_train, y_train)
+        predicted_values = model.predict(X_test)
 
-        rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
-        rf_regressor.fit(X_train, y_train)
+        dataset.loc[dataset[column].isnull(), column] = predicted_values
 
-        predicted_values = rf_regressor.predict(X_pred)
+    for column in categorical_columns:
+        model = RandomForestClassifier(n_estimators=100, random_state=42)
+        train_data = dataset[dataset[column].notnull()]
+        test_data = dataset[dataset[column].isnull()]
 
-        processed_dataset.loc[processed_dataset[column].isnull(), column] = predicted_values
-    
-    processed_dataset.to_csv(f'{file_location}/filled/{file_name}_rfregressor_and_missing_flags_filled.csv', index=False)
+        X_train = train_data.drop(columns=[column, target_column])
+        y_train = train_data[column]
 
+        X_test = test_data.drop(columns=[column, target_column])
+
+        model.fit(X_train, y_train)
+        predicted_values = model.predict(X_test)
+
+        dataset.loc[dataset[column].isnull(), column] = predicted_values
+
+    output_path = f"{file_location}/{file_name}_filled_rf.csv"
+    dataset.to_csv(output_path, index=False)
 
 
 if __name__ == "__main__":
